3cbed62d683610b64b32f68a738302da93029145
Taylor Williams <taylor@thewilliams.net>
2018-09-04 20:13:23 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,7 +46,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     if !params[:username].empty? && !params[:password].empty?
-      user = User.find_by(username: params[:username], password: params[:password])
+      user = User.find_by(username: params[:username], password_digest: params[:password])
       if user
         session[:user_id] = user.id
         redirect '/tweets'
