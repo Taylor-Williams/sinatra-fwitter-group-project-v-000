046f19479e716334f035284d2e17749487a97d11
Taylor Williams <taylor@thewilliams.net>
2018-09-04 19:30:12 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :tweets
 
   validates :username, length: { in: 5..18 }
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   has_secure_password
 
   before_save do
