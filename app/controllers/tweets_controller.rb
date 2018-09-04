a604cf9d0e68e342c4d48ec46248ce6a35d2c91a
Taylor Williams <taylor@thewilliams.net>
2018-09-04 22:31:43 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -18,8 +18,12 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id' do
-    @tweet = Tweet.find(params[:id])
-    erb :"/tweets/show"
+    if logged_in?
+      @tweet = Tweet.find(params[:id])
+      erb :"/tweets/show"
+    else
+      redirect 'login'
+    end
   end
 
   get '/tweets/:slug/edit' do
